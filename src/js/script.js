daa58efd0bb013688259a41747f43fec1a7eaa3d
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-05-06 15:00:59 +0300

    some adjustments before connecting bot

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,18 @@
+var body = document.getElementById('body');
 var modal = document.getElementById('myModal');
 var span = document.getElementById('close');
 function showModal() {
     modal.style.display = 'flex';
+    body.style.overflow = 'hidden';
 }
 span.onclick = function () {
     modal.style.display = 'none';
+    body.style.overflow = 'visible';
 };
 window.onclick = function (event) {
     console.log(event.target);
     if (event.target == modal) {
         modal.style.display = 'none';
+        body.style.overflow = 'visible';
     }
 };
