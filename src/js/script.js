af9cd175ee65bfd6467b28022e85eb40b5f5e535
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-05-06 13:36:47 +0300

    commiting before last merges

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,14 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementByClassName("myBtn")[0];
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
+var modal = document.getElementById('myModal');
+var span = document.getElementById('close');
+function showModal() {
+    modal.style.display = 'flex';
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
+    modal.style.display = 'none';
+};
 window.onclick = function (event) {
+    console.log(event.target);
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     }
-}
+};
